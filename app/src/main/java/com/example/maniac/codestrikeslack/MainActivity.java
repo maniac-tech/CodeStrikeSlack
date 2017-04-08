55ce890d9ef20228b28204a711ab2b196e3debd8
maniac-tech <ajj_2396@yahoo.com>
2017-04-09 00:02:48 +0530

    UI changes

--- app/src/main/java/com/example/maniac/codestrikeslack/MainActivity.java
+++ app/src/main/java/com/example/maniac/codestrikeslack/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,16 +20,17 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
 
     public void post(View v){
-        final EditText message = (EditText)findViewById(R.id.editText);
+        EditText message = (EditText)findViewById(R.id.editText);
+
         final String msg = message.getText().toString();
 
         String url = "https://hooks.slack.com/services/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";
@@ -61,5 +63,9 @@ public class MainActivity extends AppCompatActivity {
 
         //Add to queue:
         queue.add(jsonRequest);
+
+        //clearing the message in the textview:
+        message.setText(" ");
+
     }
 }
